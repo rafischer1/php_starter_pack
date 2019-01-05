1602eb2a581ea472249edab951f2862c6c64e506
Robert Fischer <artiefischer@gmail.com>
2019-01-05 12:27:30 -0700

    arrays

--- README.md
+++ README.md
@@ -8,6 +8,7 @@ to run on localhost:8000/hello.php
 php -S localhost:8000
 
 <?php
-  // Code will end here ?> This is output as literal text.
+  // Code will end here 
+  ?> This is output as literal text.
 
 ```
--- hello.php
+++ hello.php
@@ -1,9 +1,11 @@
 <html>
  <head>
   <title>PHP Test</title>
+  <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
   <style>
   body {
     text-align: center;
+    font-family: 'Montserrat', sans-serif;
     color: white;
     text-shadow: 2px 2px 2px rgba(255, 113, 218, 1);
     background-color: darkgrey;
@@ -14,12 +16,39 @@
     border-radius: 30px;
     color: black;
     background-color: white;
-    height: 10%;
+    height: auto;
+    padding: 2%;
   }
   </style>
  </head>
  <body>
  <?php echo '<h1>Learning PHP</h1>'; ?> 
- <?php echo '<div class="blockDiv"><p>PHP Playground</p></div>'; ?>
+ <div class="blockDiv">
+ <?php echo '<p>PHP Playground</p>'; ?>
+ <?php $sentence = 'I am a string! followed by an int:'; $num = 37; echo $sentence, $num ?>
+ <br />
+ <br />
+ <?php echo "Arrays: <br />";
+  var_dump($array = array(
+     "foo" => "bar",
+      "Artie" => "Fischer",
+      100   => -100,
+      -100  => 100,
+ ));
+echo '<br /> Accessing by key ["Artie"]: ';
+ var_dump($array["Artie"]);
+
+ echo '<br /> Two item array - no keys: ';
+ $array2 = array("hello", "world");
+   var_dump($array2);  
+
+   // methods on arrays forEach
+echo '<br /><br /> For each in an array: ';
+$colors = array('red', 'blue', 'teal', 'salmon');
+foreach ($colors as $color) {
+    echo "Color is: $color.\n";
+}
+?>
+ </div>
  </body>
 </html>
